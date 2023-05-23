787f652fdf93e02a4f48632f9320ca3b010dcea4
zerochips <zerochips123@gmail.com>
2023-05-23 18:23:30 +0900

    ocr

--- pyocr/ocr1.py
+++ pyocr/ocr1.py
@@ -4,7 +4,16 @@ from pytesseract import pytesseract
 
 # 엔진
 pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR/tesseract.exe"
-img = Image.open("source/news.PNG")
+
+# img = Image.open("source/news.PNG")
+img = Image.open("source/news2.PNG")
 # img.show()
-text = pytesseract.image_to_string(img, lang="kor")
-print(text)
+text = pytesseract.image_to_string(img, lang="kor+eng")
+# print(text)
+print(text.replace(" ", ""))    # 공백 제거
+
+# 변환된 텍스트를 파일에 쓰기
+# encoding = 'utf-8' 꼭 명시해야 함
+with open("./output/news2.text", 'w', encoding='utf-8') as f:
+    # f.write(text)
+    f.write(text.replace(" ", ""))
---
+++ pyocr/pillow1.py
@@ -0,0 +1,5 @@
+from PIL import Image
+
+img = Image.open("source/coffee-gray.jpg")
+img.show()
+
